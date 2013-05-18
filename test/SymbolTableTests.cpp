ec2be73592ed2a0108911e1650387593a05d692a
Bobby Vandiver <bobby.vandiver88@gmail.com>
2013-05-18 17:08:31 -0400

    Clean up after SymbolTableTests

--- test/SymbolTableTests.cpp
+++ test/SymbolTableTests.cpp
@@ -12,26 +12,29 @@ TEST(SymbolTableTests, IsEmptyReturnsTrueWhenNoSymbolsInTable) {
 TEST(SymbolTableTests, IsEmptyReturnsFalseForNonEmptyTable) {
 	SymbolTable symbolTable = SymbolTable();
 	Symbol *symbol = new Symbol("foo", 1234);
+	ASSERT_TRUE(symbol != NULL);
+
 	symbolTable.insert(symbol);
 	EXPECT_FALSE(symbolTable.isEmpty());
+	delete symbol;
 }
 
 TEST(SymbolTableTests, FindByNameReturnsNullWhenSymbolNotFound) {
 	SymbolTable symbolTable = SymbolTable();
 	Symbol *symbol = symbolTable.findByName("foo");
 	EXPECT_EQ(NULL, symbol);
+	delete symbol;
 }
 
 TEST(SymbolTableTests, FindByNameReturnsSymbolWithOneSymbolInTable) {
 	SymbolTable symbolTable = SymbolTable();
 	Symbol *symbol = new Symbol("foo", 1234);
+	ASSERT_TRUE(symbol != NULL);
+
 	symbolTable.insert(symbol);
-
 	Symbol *result = symbolTable.findByName("foo");
-	ASSERT_TRUE(result != NULL);
-
-	unsigned long address = result->getAddress();
-	EXPECT_EQ(1234, address);
+	EXPECT_EQ(symbol, result);
+	delete symbol;
 }
 
 TEST(SymbolTableTests, FindByNameReturnsSymbolWithMultipleSymbolsInTable) {
@@ -40,14 +43,17 @@ TEST(SymbolTableTests, FindByNameReturnsSymbolWithMultipleSymbolsInTable) {
 	Symbol *symbol1 = new Symbol("foo", 1234);
 	Symbol *symbol2 = new Symbol("bar", 567890);
 
+	ASSERT_TRUE(symbol1 != NULL);
+	ASSERT_TRUE(symbol2 != NULL);
+
 	symbolTable.insert(symbol1);
 	symbolTable.insert(symbol2);
 
 	Symbol *result = symbolTable.findByName("bar");
-	ASSERT_TRUE(result != NULL);
+	EXPECT_EQ(symbol2, result);
 
-	unsigned long address = result->getAddress();
-	EXPECT_EQ(567890, address);
+	delete symbol1;
+	delete symbol2;
 }
 
 TEST(SymbolTableTests, InsertNullThrowsException) {
@@ -57,10 +63,16 @@ TEST(SymbolTableTests, InsertNullThrowsException) {
 
 TEST(SymbolTableTests, InsertDuplicateSymbolThrowsException) {
 	SymbolTable symbolTable = SymbolTable();
+
 	Symbol *symbol1 = new Symbol("foo", 1234);
 	Symbol *symbol2 = new Symbol("foo", 1234);
+
+	ASSERT_TRUE(symbol1 != NULL);
+	ASSERT_TRUE(symbol2 != NULL);
+
 	symbolTable.insert(symbol1);
 	EXPECT_THROW(symbolTable.insert(symbol2), IllegalArgumentException);
+
 	delete symbol1;
 	delete symbol2;
 }
@@ -74,9 +86,13 @@ class MockSymbol : public Symbol {
 
 TEST(SymbolTableTests, DestructorDeletesAllSymbols) {
 	SymbolTable *symbolTable = new SymbolTable();
+	ASSERT_TRUE(symbolTable != NULL);
+
 	MockSymbol *mockSymbol = new MockSymbol;
 	EXPECT_CALL(*mockSymbol, Die());
+
 	symbolTable->insert(mockSymbol);
+
 	delete symbolTable;
 	delete mockSymbol;
 }
