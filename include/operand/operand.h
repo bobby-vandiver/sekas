792512556a277df90ab900ab06580721d8dbef91
Bobby Vandiver <bobby.vandiver88@gmail.com>
2016-10-12 22:52:01 -0400

    Operand methods should not modify state

--- include/operand/operand.h
+++ include/operand/operand.h
@@ -7,11 +7,11 @@ class operand {
 public:
     virtual ~operand() {}
 
-    virtual uint8_t get_register_field() = 0;
-    virtual uint8_t get_mode_field() = 0;
+    virtual uint8_t get_register_field() const = 0;
+    virtual uint8_t get_mode_field() const = 0;
 
-    virtual uint8_t get_extension_word_count() = 0;
-    virtual uint16_t get_extension_word(const uint8_t idx) = 0;
+    virtual uint8_t get_extension_word_count() const = 0;
+    virtual uint16_t get_extension_word(const uint8_t idx) const = 0;
 };
 
 #endif
